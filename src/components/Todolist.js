476d9fb3c501b695266d5e1e1e768009608a1069
Adeoluwa <minneade@gmail.com>
2021-02-02 16:44:30 +0100

    new

---
+++ src/components/Todolist.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Todo from './Todo'
+
+function Todolist ({todos,setTodos, filteredTodos}){
+    return(
+        <div className="todo-container">
+            <ul className="todo-list">
+               {filteredTodos.map(todo => (
+                   <Todo 
+                   todos={todos}
+                   setTodos={setTodos}
+                   todo={todo}
+                   key={todo.id}
+                   text={todo.text}/>
+               ))}
+            </ul>
+        </div>
+    )
+}
+
+export default Todolist
